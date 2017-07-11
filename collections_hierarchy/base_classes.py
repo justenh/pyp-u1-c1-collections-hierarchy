fb53523be493de215d1a0abd305cc8d75505b4e2
Justen Holter <justen@fromsomebodyelse.com>
2017-07-11 08:55:49 -0500

    Add Sequencable, Appendable, Popable,  and Pushable

--- collections_hierarchy/base_classes.py
+++ collections_hierarchy/base_classes.py
@@ -2,23 +2,58 @@ from .node import Node
 
 
 class Sequenceable(object):
-    def __init__(self):
-        pass
+    def __init__(self, start = None):
+        self.start = start
 
     def get_elements(self):
-        pass
+        """Returns an array of all Node values in the stack"""
+        node = self.start
+        values = []
 
+        while node is not None:
+            values.append(node.value)
+            node = node.next
+            
+        return values
+
+    def get_end(self):
+        """Returns the last element in the stack"""
+        lastNode = self.start
+        nextNode = self.start
+        while nextNode is not None:
+            lastNode = nextNode
+            nextNode = lastNode.next
+        return lastNode
+    
 
 class Appendable(object):
     def append(self, element):
-        pass
+        """Adds a node to the end of the stack"""
+        last = self.get_end()
+        if last is None:
+            self.start = Node(element)
+        else:
+            last.next = Node(element)
 
 
 class Popable(object):
     def pop(self):
-        pass
+        """Removes the first node from the stack"""
+        first = self.start
+
+        if first is None:
+            raise IndexError()
+        else if first.next is not None:
+            self.start = first.next
+        else:
+            self.start = None;
+
+        return first.value;
 
 
 class Pushable:
     def push(self, element):
-        pass
+        """Inserts a node at the start of the stack"""
+        first = Node(element, self.start)
+        self.start = first
+        
